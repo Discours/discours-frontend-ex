289b4cb5d246e157dcbf465b7f131addd355dc96
Alexander Gusev <goooseman@me.com>
2019-08-04 08:08:29 +0300

    test(Typography): 💍  Create test for a component

---
+++ src/components/discours-ui-kit/Typography/Typography.spec.tsx
@@ -0,0 +1,31 @@
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, render } from "@testing-library/react";
+import * as React from "react";
+import Typography from "./Typography";
+
+afterEach(cleanup);
+
+it("should forward className", async () => {
+  const { getByText } = render(<Typography className="test">Test</Typography>);
+  const text = getByText("Test");
+  expect(text).toHaveClass("test");
+});
+
+it("should render p tag by default", async () => {
+  const { getByText } = render(<Typography>Test</Typography>);
+  const text = getByText("Test");
+  expect(text).toBeInstanceOf(HTMLParagraphElement);
+  expect(text).toBeInTheDocument();
+  expect(text).toHaveClass("body");
+});
+
+it("should render h3 for heading3 variant", async () => {
+  const { getByText } = render(
+    <Typography variant="heading3">Test</Typography>,
+  );
+  const text = getByText("Test");
+  expect(text).toBeInstanceOf(HTMLHeadingElement);
+  expect(text).toBeInTheDocument();
+  expect(text).toHaveClass("heading");
+  expect(text).toHaveClass("heading3");
+});
