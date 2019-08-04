e9aa7cbe05225026b6271dcec4ed04f0193f1f4a
Alexander Gusev <goooseman@me.com>
2019-08-04 07:36:12 +0300

    design(Typography): 💄  Starting to describe Typography component

--- src/components/discours-ui-kit/Typography/Typography.tsx
+++ src/components/discours-ui-kit/Typography/Typography.tsx
@@ -1,10 +1,42 @@
-import * as React from "react";
+import classnames from "classnames";
+import * as React from "react"; // TODO
 import classes from "./Typography.css";
 
-class Typography extends React.Component {
+interface Props {
+  variant?: "body" | "heading3";
+  className?: string;
+}
+
+class Typography extends React.Component<Props> {
   public render() {
-    return <p className={classes.text}>{this.props.children}</p>;
+    const TagName = this.getTagName();
+    const className = this.getClassName();
+    return <TagName className={className}>{this.props.children}</TagName>;
   }
+
+  private getTagName = () => {
+    const { variant } = this.props;
+    switch (variant) {
+      case "body":
+        return "p";
+      case "heading3":
+        return "h3";
+      default:
+        return "p";
+    }
+  };
+
+  private getClassName = () => {
+    const { variant = "body", className = "" } = this.props;
+
+    return classnames({
+      [classes.common]: true,
+      [classes.body]: variant === "body",
+      [classes.heading]: variant.match(/^heading/),
+      [classes.heading3]: variant === "heading3",
+      [className]: true,
+    });
+  };
 }
 
 export default Typography;
